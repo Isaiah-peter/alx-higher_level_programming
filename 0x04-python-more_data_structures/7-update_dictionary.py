495238ea9044343a4f13504af94b30e4b13517e4
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-07-07 01:03:00 +0100

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = {}
-    if key not in list(a_dictionary):
-        a_dictionary[key] = value
-    for k, v in a_dictionary.items():
-        if k == key:
-            new_dict[key] = value
-        else:
-            new_dict[k] = v
-    return new_dict
+    a_dictionary[key] = value
+    return (a_dictionary)

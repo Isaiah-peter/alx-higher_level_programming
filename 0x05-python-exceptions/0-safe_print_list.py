5fef8a540179aa9ff488d1d6111bf71d90aea6a2
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-07-18 22:13:48 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             num = num + 1
     except:
-        "nice"
-    print("\n")
+        None
+    print()
 
     return num

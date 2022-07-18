5b3fa19790ecb2985d6b9fa0bebcda7da6f3b90c
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-07-19 00:43:59 +0100

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        except (TypeError, ValueError, IndexError, ZeroDivisionError):
+        except (TypeError, ValueError, IndexError, ZeroDivisionError, Exception):
             result = b + a
             break
     return result

8ef58179cd0149ee17cb94bd4be824f9e55f93ba
Isaiah peter <peterisaiah4fun@gmail.com>
2022-07-25 15:56:02 -0700

    more on classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""A Rectangle Class"""
+
+class Rectangle:
+    """Empty Rectangle class"""
+    pass
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""A Rectangle Class"""
+
+class Rectangle:
+    """
+    Rectangle class with instance method width
+    and height and instance Attribute
+    width and height
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+    
+    def perimeter(self):
+        return (self.__height + self.__width) * 2
+
+    @property
+
+    def height(self):
+        return self.__height
+
+    @height.setter
+
+    def height(self, height):
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
+    @property
+
+    def width(self):
+        return self.__width
+
+    @width.setter
+
+    def width(self, width):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise TypeError("width must be >= 0")
+        else:
+            self.__width = width
+

6b19850558088d0b749b907a63f729ee10d74f4a
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-07-26 00:02:02 +0100

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,16 +9,14 @@ class Rectangle:
     width and height
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
-
     def height(self):
         return self.__height
 
     @height.setter
-
     def height(self, height):
         if type(height) != int:
             raise TypeError("height must be an integer")
@@ -28,12 +26,10 @@ class Rectangle:
             self.__height = height
 
     @property
-
     def width(self):
         return self.__width
 
     @width.setter
-
     def width(self, width):
         if type(width) != int:
             raise TypeError("width must be an integer")

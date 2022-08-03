cb7a3a9ae96a55b4216e8b6c4002e9fd3f235c39
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-08-03 01:50:38 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,5 @@
 
 
 def write_file(filename="", text=""):
-    string = str(text)
-    with open(filename, 'r+', encoding="utf-8") as f:
-        return f.write(string)
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)

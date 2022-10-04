d74290f6eaf1fa84227130e276555f3499df5fc6
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-04 22:27:31 +0100

    task 1 - 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if(h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let m = '';
+    for(let i = 0; i < this.height; i++) {
+      for(let j = 0; j < this.width; i++) {
+        m += 'X';
+      }
+
+      if(i != this.height-1) {
+        m += '\n'
+      }
+    }
+
+    console.log(m);
+  }
+}
+
+module.exports = Rectangle;

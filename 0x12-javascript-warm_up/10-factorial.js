40b4b049179b8320ff13734b17b31a8837472f6b
Isaiah peter <peterisaiah4fun@gmail.com>
2022-10-04 02:28:42 +0000

    more

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,16 +4,12 @@ const process = require('process');
 
 const argv = process.argv;
 
-function factorial(n) {
-  if (n == NaN) {
-    console.log(1);
-    return;
-  }
-  if (n == 1) {
-    return 1
+function factorial (n) {
+  if (n === 1) {
+    return 1;
   }
 
-  return n * factorial(n-1)
+  return n * factorial(n - 1);
 }
 
-console.log(factorial(argv[2]))
+console.log(factorial(argv[2]));

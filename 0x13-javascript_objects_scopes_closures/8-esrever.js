0bd3a36f9e044bc6a1c5be1030f1b023180388f1
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-04 23:59:00 +0100

    morem

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,9 @@
 exports.esrever = function (list) {
   let arr = [];
-
-  console.log(list.length - 1)
+  
+  for(let i = list.length - 1; i > 0; i++) {
+    arr.push(list[i])
+  }
 
   return arr;
 }

c36c7dc620e217b828e7528fff4db4121ec01146
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-04 03:22:17 +0100

    more

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -11,11 +11,9 @@ function factorial(n) {
   }
   if (n == 1) {
     return 1
-  } else if (n == 2) {
-    return 2
   }
 
-  return factorial(n-1) * factorial(n-2)
+  return n * factorial(n-1)
 }
 
 console.log(factorial(argv[2]))

ca6377ed2c0e1bcb7447a4cdcd521daf304a7ff4
Isaiah peter <peterisaiah4fun@gmail.com>
2022-10-08 23:47:40 +0100

    work

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,14 +3,15 @@
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost", port=3306,
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306,
                      user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
-cur = db.cursor()
+    cur = db.cursor()
 
-cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(f"{row}")
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"{row}")
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,18 +3,17 @@
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost", port=3306,
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
                      user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
 
-cur = db.cursor()
-cur.execute("SELECT id, name FROM states WHERE name =%(name)s ORDER BY id ASC",
-            {"name": argv[4]})
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states WHERE name =%(name)s ORDER BY id ASC",
+                {"name": argv[4]})
 
-rows = cur.fetchall()
+    rows = cur.fetchall()
 
-for row in rows:
-    print(f"{row}")
-
-
-cur.close()
-db.close()
+    for row in rows:
+        print(f"{row}")
+    cur.close()
+    db.close()

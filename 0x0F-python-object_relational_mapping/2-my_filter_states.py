fc4ba24ca9da65ad82dd9bb32f7b0f8f6c4870e0
Isaiah peter <peterisaiah4fun@gmail.com>
2022-10-08 22:52:02 +0100

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+cur = db.cursor()
+cur.execute("SELECT id, name FROM states WHERE name ='%s'" % (argv[4]))
+
+rows = cur.fetchall()
+
+for row in rows:
+    print (f"{row}")
+
+
+cur.close()
+db.close()

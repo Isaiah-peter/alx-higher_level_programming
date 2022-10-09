63b6892335a7aa3e8f691dc2687d86e628af94de
Isaiah peter <peterisaiah4fun@gmail.com>
2022-10-10 00:38:10 +0100

    please work

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-find state that have a
+Get script that prints the first State object from
+the database hbtn_0e_6_usa
 """
 
 from sys import argv
 
+from sqlalchemy import (create_engine)
+
 from model_state import State
 from sqlalchemy.orm import sessionmaker
-
-
-from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
 
     session = Session()
-
-    rows = session.query(State.id, State.name).filter(State.name.like('%a%')).order_by(State.id)
-
-    for row in rows:
-        print(f"{row.id}: {row.name}")
+    row = session.query(State.id, State.name).filter(State.name.is_("%s".format("Texas")))
+    if row is None:
+        print("Not found")
+    else:
+        print(f"{row}")

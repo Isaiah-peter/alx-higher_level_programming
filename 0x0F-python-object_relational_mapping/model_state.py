1859fbdf3cede655d9ff34ff6d499b50327d4467
Isaiah peter <peterisaiah4fun@gmail.com>
2022-10-09 22:25:26 +0100

    please work

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,7 +12,7 @@ class State(Base):
     """States"""
     __tablename__ = "states"
     id = Column(Integer,nullable=False ,primary_key=True)
-    name = Column(String, nullable=False)
+    name = Column(String(128), nullable=False)
 
     def __repr__(self):
         return "<State name = %(name)s, >" % {self.name}

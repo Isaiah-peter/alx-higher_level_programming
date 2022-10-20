433b9e59aa809e2497893f23afa449713ca01650
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-20 22:25:04 +0100

    work

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script that takes in a URL and return content:type
-echo $(curl $1 -sI | grep -i Content-Length | awk '{print $2}')
+echo $(curl $1 -s -I | grep -i Content-Length | awk '{print $2}')

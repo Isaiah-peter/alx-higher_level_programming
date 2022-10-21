a844157a9d11dfc03e80d84736ce17cb145c98f1
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-21 12:38:38 +0100

    task 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from urllib import request
+
+with request.urlopen("https://alx-intranet.hbtn.io/status") as respond:
+    print("Body response:")
+    print(f"    - type: {type(respond)}")
+    print(f"    - content: b'{respond.__dict__.get('msg')}'")
+    print(f"    - utf8 content: {respond.__dict__.get('msg')}")

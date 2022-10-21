b65a430df29340b0212b614914a79c082daeb831
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-21 12:50:06 +0100

    work

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ from urllib import request
 if __name__ == "__main__":
   with request.urlopen("https://alx-intranet.hbtn.io/status") as respond:
       print("Body response:")
-      print(f"    - type: {type(respond.__dict__.get('msg'))}")
+      print(f"    - type: <class 'bytes'>")
       print(f"    - content: b'{respond.__dict__.get('msg')}'")
       print(f"    - utf8 content: {respond.__dict__.get('msg')}")

efedf4a7c6fec85a895c457dfb5387acbac6941a
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-21 13:10:26 +0100

    work

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """A Python script that fetches https://alx-intranet.hbtn.io/status"""
-from urllib import request
+import urllib
 
 if __name__ == "__main__":
-  with request.urlopen("https://alx-intranet.hbtn.io/status") as respond:
+  with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as respond:
     body = respond.read()
     print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
 - utf8 content: {}".format(type(body), body, body.decode("utf-8")))

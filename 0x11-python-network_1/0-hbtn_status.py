a661e65c568b9e03dab0e62ebce8314ea7a5fbc4
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-21 12:47:10 +0100

    work

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """A Python script that fetches https://alx-intranet.hbtn.io/status"""
 
 from urllib import request
+
 with request.urlopen("https://alx-intranet.hbtn.io/status") as respond:
     print("Body response:")
     print(f"    - type: {type(respond.__dict__.get('msg'))}")

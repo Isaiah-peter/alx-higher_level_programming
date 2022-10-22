d5f87a85f137b7b61113c14c36ee24ffb6aadf83
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-22 23:03:09 +0100

    task 5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,4 +5,4 @@ import requests
 if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:\n\t- type: {}\n\t- content: {}\
-".format(type(r.reason), r.reason))
+".format(type(r.text), r.text))

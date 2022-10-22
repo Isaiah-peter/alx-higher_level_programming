65a0d2c954c30a4c4d084bc788f0200226856c2f
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-22 23:32:29 +0100

    task 8

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" sends a post request to the URL and displays the body
+"""
+from urllib.error import HTTPError
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    if r.status_code == 200:
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))

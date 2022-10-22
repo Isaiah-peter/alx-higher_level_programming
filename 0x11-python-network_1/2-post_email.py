f7370c79b6ef317c5e15cc3d92464faa211d18da
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-22 21:47:35 +0100

    task 3

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+ython script that takes in a
+URL and an email, sends aPOST request to
+the passed URL with
+the email as a parameter, and displays
+the body of the respons
+"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+url = argv[1]
+value = {"email": argv[2]}
+
+data = urllib.parse.urlencode(value)
+data = data.encode("ascii")
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as respond:
+    body = respond.read()
+    print(body.decode("utf8"))

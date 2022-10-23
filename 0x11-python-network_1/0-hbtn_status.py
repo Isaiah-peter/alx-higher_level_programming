5af0eadb69d03c1c8a9fe5aa268e3dadb5f9c94a
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-23 21:28:35 +0100

    fix pycodesytle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,8 @@
 import urllib.request
 
 if __name__ == "__main__":
-  with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as respond:
-    body = respond.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status\
+	") as respond:
+        body = respond.read()
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
 - utf8 content: {}".format(type(body), body, body.decode("utf-8")))

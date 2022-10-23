4a31b1be6d85a0ef922c5cbc2a9ae9baf984df90
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-23 21:06:38 +0100

    task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches id"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    repo_name = argv[1]
+    owner = argv[2]
+
+    r = requests.get(
+        "https://api.github.com/repos/{}/{}/commits".format(owner, repo_name))
+    for i in r.json():
+        print("{}: {}\
+            ".format(i.get('sha'), i.get('commit').get('author').get('name')))

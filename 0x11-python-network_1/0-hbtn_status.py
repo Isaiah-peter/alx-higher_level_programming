ec876127f5f3e333032335453b4613221a48576d
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-23 21:30:57 +0100

    fix pycode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """A Python script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
+from urllib.request import urlopen
 
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status\
-	") as respond:
+    with urlopen("https://alx-intranet.hbtn.io/status") as respond:
         body = respond.read()
         print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
 - utf8 content: {}".format(type(body), body, body.decode("utf-8")))

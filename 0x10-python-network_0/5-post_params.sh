1af01b51f2dfe24b5efeabbb09a82ac2c5f4de07
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-23 23:22:41 +0100

    pleaswork

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #use post
-curl $1 -s -X POST -d '"email":"test@gmail.com"' -d '"subject": "I will always be here for PLD"'
+curl $1 -s -X POST -d 'email:"test@gmail.com' -d 'subject: I will always be here for PLD'

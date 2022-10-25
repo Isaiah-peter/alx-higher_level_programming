ad203c7f8c1ca1e5c6473fd68d90ef0a80d4712d
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-25 23:48:12 +0100

    pleasswork

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,11 +6,12 @@ from sys import argv
 if __name__ == "__main__":
     repo_name = argv[1]
     owner = argv[2]
-    num = 0
     r = requests.get(
-        "https://api.github.com/repos/{}/{}/commits".format(owner, repo_name))
+        "https://api.github.com/repos/{}/{}/commits".format(owner, repo_name),
+        params = {
+            "per_page": 10,
+        }
+        )
     for i in r.json():
-        if num < 10:
             print("{}: {}\
             ".format(i.get("sha"), i.get("commit").get("author").get("name")))
-        num += 1

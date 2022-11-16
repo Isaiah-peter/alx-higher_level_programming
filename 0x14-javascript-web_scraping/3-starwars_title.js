624aef92c8d5facc1a025803ce4c30137ec66b79
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-11-16 05:30:16 +0100

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,5 @@
 const request = require('request');
 request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (error, response, body) {
   error && console.log(error);
-  body && console.log(body.title);
+  body && console.log(JSON.parse(body).title);
 });

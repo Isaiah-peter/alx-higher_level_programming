59ca9f336fe35ca32d18dcd707499d9b0d0d273d
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-11-16 05:47:25 +0100

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// make a GET request and print the status code
+const request = require('request');
+let count = 0;
+request(process.argv[2], function (error, response, body) {
+    error && console.log(error);
+    if (body) {
+        let result = JSON.parse(body).results;
+
+        for (i of result) {
+            if (i.characters.includes("https://swapi-api.hbtn.io/api/people/18/")) {
+                count++
+            }
+        }
+
+        console.log(count);
+    }
+});

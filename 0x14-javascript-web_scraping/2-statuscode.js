19ef75e00f14ac785aeecca4b85888f257e88804
Peter Isaiah <peterisaiah4fun@gmail.com>
2022-11-15 20:10:24 -0800

    more

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,9 +6,9 @@ const request = require('request');
 const argv = process.argv;
 
 request(argv[2], function (error, res) {
-	if (!error) {
-		console.log(res.statusCode);
-	}else {
-		console.log(res.statusCode);
-	}
-})
+  if (!error) {
+    console.log(res.statusCode);
+  } else {
+    console.log(res.statusCode);
+  }
+});

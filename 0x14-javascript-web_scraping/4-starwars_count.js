b9ca9d7d8e913d66b7e4559952a441f7ea4b4ef0
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-11-16 05:50:51 +0100

    task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,16 +3,16 @@
 const request = require('request');
 let count = 0;
 request(process.argv[2], function (error, response, body) {
-    error && console.log(error);
-    if (body) {
-        let result = JSON.parse(body).results;
+  error && console.log(error);
+  if (body) {
+    let result = JSON.parse(body).results;
 
-        for (i of result) {
-            if (i.characters.includes("https://swapi-api.hbtn.io/api/people/18/")) {
-                count++
-            }
-        }
-
-        console.log(count);
+    for (i of result) {
+      if (i.characters.includes("https://swapi-api.hbtn.io/api/people/18/")) {
+        count++
+      }
     }
+
+    count && console.log(count);
+  }
 });

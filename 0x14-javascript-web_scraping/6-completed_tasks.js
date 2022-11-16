264e144208bd27d5393f710a068c89bc5268927e
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-11-16 06:04:14 +0100

    task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// make a GET request and print the status code
+const request = require("request");
+let count = {};
+request(process.argv[2], function (error, response, body) {
+  error && console.log(error);
+  if (body) {
+    let result = JSON.parse(body);
+    result.forEach((i) => {
+      if (count[i["userId"]]) {
+        if (i.completed == true) {
+          count[i["userId"]] = count[i["userId"]] + 1;
+        }
+      } else {
+        if (i.completed == true) {
+          count[i["userId"]] = 1;
+        }
+      }
+    });
+
+    console.log(count);
+  }
+});

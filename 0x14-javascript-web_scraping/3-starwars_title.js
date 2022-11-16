8279eefe416a0e69927136b3eaaf4028317bf0a9
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-11-16 05:22:13 +0100

    please work

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// make a GET request and print the status code
+const request = require('request');
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (error, response, body) {
+  error && console.log(error);
+  body && console.log(body.title);
+});

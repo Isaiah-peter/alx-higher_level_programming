f69b20de7722a108e41351a84ce3a57b64ad1f78
Peter Isaiah <peterisaiah4fun@gmail.com>
2022-11-15 20:01:24 -0800

    more

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,6 @@
 const process = require('process');
 const fs = require('fs');
 
-
 const argv = process.argv;
 
 fs.readFile(argv[2], 'utf8', (err, data) => {
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,12 +3,11 @@
 const process = require('process');
 const fs = require('fs');
 
-
 const argv = process.argv;
-const text = argv[3]
+const text = argv[3];
 
 fs.writeFile(argv[2], text, (err) => {
-    if (err != null) {
-        console.log("file write error");
-    }
+  if (err != null) {
+    console.log('file write error');
+  }
 });

ac2a99bb78a0f21fab7783fcf6b4a5bc88e66e6b
Isaiah peter <peterisaiah4fun@gmail.com>
2022-12-19 14:58:43 +0100

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,5 +4,4 @@
 
 def inherits_from(obj, a_class):
     """check if class inherited from"""
-    return issubclass(type(obj), a_class)
-    
+    return isinstance(obj, a_class) and type(obj) != a_class

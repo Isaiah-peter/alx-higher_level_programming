f7841e19f20a889b366a5b783e9e2624b53aaec1
Isaiah peter <peterisaiah4fun@gmail.com>
2022-12-19 15:10:06 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""create a new Class called BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry"""
+    def area(self):
+        """area"""
+        raise Exception("area() is not implemented")
